b6ff287af92edf59333c4d2a32a0b501a30ef99e
Markus Leyendecker <markus.c.leyendecker@gmail.com>
2015-09-07 15:04:20 +0200

    Learned about class methods in databases

--- 01-First-Rails-Project/timetracking/app/models/test.rb
+++ 01-First-Rails-Project/timetracking/app/models/test.rb
@@ -6,4 +6,9 @@ class Test < ActiveRecord::Base
   def self.last_updated(num)
     limit(num).order(updated_at: :desc)
   end
+
+  def self.clean_old
+    time = Time.now - 1.minutes
+    where('created_at < ?', time).destroy_all
+  end
 end
