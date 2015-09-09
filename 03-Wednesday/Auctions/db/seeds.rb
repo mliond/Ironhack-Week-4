afb5148ba0d1d29aba0bbaad8d232d0b6aadadd1
Markus Leyendecker <markus.c.leyendecker@gmail.com>
2015-09-09 21:11:35 +0200

    some progress on the auctions

--- 03-Wednesday/Auctions/db/seeds.rb
+++ 03-Wednesday/Auctions/db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.all.destroy_all
+Product.all.destroy_all
+
+puts "Deleted all users and products"
+
+num = 5
+
+num.times do |i|
+  u = User.new({
+    name: Faker::Name.name,
+    email: Faker::Internet.email
+    })
+  u.save
+  3.times do |k|
+    u.products.create({
+      title: Faker::Hacker.abbreviation,
+      description: Faker::Hacker.say_something_smart,
+      deadline: Faker::Time.between(DateTime.now + 10, DateTime.now),
+      user_id: i
+      })
+  end
+end
+
+
+puts "Created #{num} new users with 3 entries each."
