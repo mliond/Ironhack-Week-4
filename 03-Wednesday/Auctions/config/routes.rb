6932fef1e60b710e291919fa7b2a41b8aa0a293e
Markus Leyendecker <markus.c.leyendecker@gmail.com>
2015-09-10 13:55:47 +0200

    starting auctions from scratch

--- 03-Wednesday/Auctions/config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-
-  resources 'products', only: [:index, :show, :edit, :update]
-
-  resources 'users' do
-    resources 'products', only: [:new, :create]
-  end
-
-end
