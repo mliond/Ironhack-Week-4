55560e53d139db98869d57e2e64e717aca7a1a40
Markus Leyendecker <markus.c.leyendecker@gmail.com>
2015-09-11 11:23:49 +0200

    first Bootstrap iteration stable

--- 05-Friday/Rock with Rails/app/controllers/concerts_controller.rb
+++ 05-Friday/Rock with Rails/app/controllers/concerts_controller.rb
@@ -1,21 +1,25 @@
 class ConcertsController < ApplicationController
 
   def index
+    @heading = "All concerts"
     @concerts = Concert.all
   end
 
   def concerts_today
+    @heading = "Concerts today"
     @concerts = Concert.where("date < ?", Date.today+1)
     render :index
   end
 
   def concerts_month
+    @heading = "Concerts this month"
     @concerts = Concert.where("date > ?", Date.today+1)
     render :index
   end
 
-  def concerts_budget(budget)
-    @concerts = Concert.where("price < ?", budget)
+  def concerts_budget
+    @heading = "Concerts under 10€"
+    @concerts = Concert.where("price < ?", 10)
     render :index
   end
 
--- 05-Friday/Rock with Rails/config/routes.rb
+++ 05-Friday/Rock with Rails/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   get '/concerts/today' => 'concerts#concerts_today'
   get '/concerts/month' => 'concerts#concerts_month'
+  get '/concerts/budget' => 'concerts#concerts_budget'
   resources 'concerts'
 
 end
